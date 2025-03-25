f6d8ddde5e3f189f803ece6a23a165de89ef7295
Maximilian <stramaxim@gmail.com>
2025-03-25 07:55:21 +0100

    Implement server functionality and add client placeholder; update main program output

--- main/main.c
+++ main/main.c
@@ -1,11 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+// Config
+
+
+int port = 187; // port to listen on server
+
+// Config end
+
+
 
 int Server() {
-    printf("Server\n");
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket creation failed");
+        return -1;
+    }
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(port);
+
+    // Binding the socket to the port
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+        perror("bind failed");
+        close(sockfd);
+        return -1;
+    }
+
+    // Listening for incoming connections
+    if (listen(sockfd, 5) == -1) {
+        perror("listen failed");
+        close(sockfd);
+        return -1;
+    }
+
+    printf("Server is listening on port %d\n", port);
+
+
+
+
+
+
+
     return 0;
 }
 
-
+int Client() {
+    printf("Start Client\n");
+    return 0;
+}
 
 
 
@@ -13,12 +64,12 @@ int Server() {
 int main() {
     int mode;
     printf("\n\n\n\n\n\n\n\n\n");
-        printf("  _____       _____ _           _    _____               \n");
-        printf(" / ____|     / ____| |         | |  / ____|              \n");
-        printf("| |   ______| |    | |__   __ _| |_| |     ___  _ __ ___ \n");
-        printf("| |  |______| |    | '_ \\ / _` | __| |    / _ \\| '__/ _ \\\n");
-        printf("| |____     | |____| | | | (_| | |_| |___| (_) | | |  __/\n");
-        printf(" \\_____|     \\_____|_| |_|\\__,_|\\__|\\_____\\___/|_|  \\___|\n");
+        printf("  _____       _____ _           _    _____                          \n");
+        printf(" / ____|     / ____| |         | |  / ____|                         \n");
+        printf("| |   ______| |    | |__   __ _| |_| |     ___  _ __ ___            \n");
+        printf("| |  |______| |    | '_ \\ / _` | __| |    / _ \\| '__/ _ \\        \n");
+        printf("| |____     | |____| | | | (_| | |_| |___| (_) | | |  __/           \n");
+        printf(" \\_____|     \\_____|_| |_|\\__,_|\\__|\\_____\\___/|_|  \\___|    \n");
         printf("V0.005\n");
         printf("Please visit our Website vor more Information:\n maximalst.de \n\n\n");
 
